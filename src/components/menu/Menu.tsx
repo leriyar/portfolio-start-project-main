d0c7e171f47c10caa676179649c223cc10e7e0ed
leriyar <leriyar@gmail.com>
2024-11-14 01:18:21 +0100

    Header, main created

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Menu = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Works</a></li>
+                <li><a href="">Testimony</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from "styled-components";
+import photo from '../../../Assets/images/photo.jpg'
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Valeryia Yarmak</Name>
+                    <MainTitle>A Web Developer.</MainTitle>
+                </div>
+
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: bisque;
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+
+const MainTitle = styled.h1 `
+
+`
+
+const Name = styled.h2 `
+    
+`
